713e85a3f042d9791dedacf65fddb731c5d633d3
ByteBlitz <64800402+byteblitz@users.noreply.github.com>
2022-11-11 01:04:40 +0100

    Finished the rework. I screwed up the runtime constants though

--- Main.py
+++ Main.py
@@ -53,7 +53,7 @@ class Post:
     # all posts are of similar quality and similarly convincing
 
     def __init__(self, bias):
-        #
+        # properties
         self.fake_bias = as_probability(np.random.normal(bias, 0.1))
         # TODO: come up with a better standard deviation
         self.creation = timestamp
@@ -77,15 +77,15 @@ class Post:
 
 
 class Subreddit:
-    def __init__(self, bias, bias_dev, tolerance, tolerance_dev):
+    def __init__(self, bias, tolerance):
         # post queues
         self.posts = 0
         self.hot = []
         self.new = []
 
-        # stats
-        self.bias = as_probability(np.random.normal(bias, bias_dev))
-        self.tolerance = as_probability(np.random.normal(tolerance, tolerance_dev))
+        # properties
+        self.bias = as_probability(np.random.normal(bias, 0.2))
+        self.tolerance = as_probability(np.random.normal(tolerance, 0.2))
 
         # statistics
 
@@ -93,7 +93,6 @@ class Subreddit:
         self.posts += 1
         self.new.append(post)
         self.hot.append(post)
-        self.hot.sort(key=get_hot)
         # amortize sorting by either using a binary heap or splitting time steps into insertion, then sort
 
 
@@ -102,208 +101,165 @@ class User:
     # everyone is equally smart
     # only upvotes/downvotes as regulators
     # repetition effect not taken into account, introduce vulnerability variable
+    def __init__(self, fake_bias, creator_bias, touch_grass_bias):
+        # properties
+        self.fake_bias: float = as_probability(np.random.normal(fake_bias, 0.2))
+        self.creator_bias: float = as_probability(np.random.normal(creator_bias, 0.2))
+        self.touch_grass_bias: float = as_probability(np.random.normal(touch_grass_bias, 0.2))
+        self.subreddits = []
+        # TODO: convert to array of arrays of subreddits, the bias towards the subreddit and the respective positions in the hot/new list
 
-    # TODO: define types
-
-    # the range of fakeness an agent likes to enjoy his news
-    fake_bias: float
-    # probability not to be online
-    touch_grass_bias: float
-    # how likely an agent is to create a post in a given round
-    creator_bias: float
-
-    # statistics
-    viewed_posts = 0
-    created_posts = 0
-
-    # check_bias = 0.0
-
-    def __init__(self, fake_bias, creator_bias, check_bias):
-        self.fake_bias = fake_bias
-        self.creator_bias = creator_bias
-        self.in_post = []
-        # self.check_bias = check_bias
-
-    def __init__(self, fake_bias, fake_dev, creator_bias, creator_dev, check_bias, check_dev):
-        self.fake_bias = as_probability(np.random.normal(fake_bias, fake_dev))
-        self.creator_bias = as_probability(np.random.normal(creator_bias, creator_dev))
-        self.in_post = []
-        # self.check_bias = as_probability(np.random.normal(check_bias, check_dev))
+        # statistics
+        self.viewed_posts: int = 0
+        self.created_posts: int = 0
 
     # function that evaluates if a user agrees to a post
     def agree(self, post, threshold):
-
+        """Evaluates, whether the user agrees with a post enough, to change their opinion"""
         # TODO: get me a proper function of agreement
         return abs(self.fake_bias - post.fake_bias) < threshold
 
     def disagree(self, post, threshold):
-
+        """Evaluates, whether the user disagrees with a post enough, to change their opinion"""
         # TODO: get me a proper function of disagreement
         return abs(self.fake_bias - post.fake_bias) > (1 - threshold)
 
     def vote(self, post):
-
-        # TODO: include ignore
         if self.agree(post, 0.2):
-            post.upvotes += 1
+            post.ups += 1
         elif self.disagree(post, 0.2):
-            post.upvotes -= 1
+            post.downs += 1
 
     def new_bias(self, post):
-
-        # TODO: include ignore
         if self.agree(post, 0.1):
             # post can inc one's fake bias maximum 20%
             self.fake_bias = max(min((self.fake_bias * 5 + post.fake_bias) / 6, 1), 0)
-        else:
+        elif self.disagree(post, 0.1):
             # post can dec one's fake bias by maximum 16%
             self.fake_bias = max(min((self.fake_bias * 7 - post.fake_bias) / 6, 1), 0)
 
     def create_post(self):
-
         self.created_posts += 1
-
-        # make post
         post = Post(self.fake_bias)
 
-        # transmit post
+        # select up to 3 subreddits to post on
+        for subreddit in random.choices(self.subreddits, k=random.randint(1, min(3, len(self.subreddits)))):
+            subreddit.enqueue(post)
+
         return post
 
     def consume_post(self):
+        # select subreddit
+        # TODO: add option to stay on a subreddit
+        subreddit = random.choice(self.subreddits)
 
-        # take (random) post out of the agent's buffer
-        while len(self.in_post) != 0:
-            post = self.in_post.pop()
-
-            if post is None:
-                continue
-
-            self.viewed_posts += 1
-            post.views += 1
+        # take the first 5 posts from the subreddits "hot" queue
+        # TODO: select between hot/new also keep position in queue for continuous scrolling
+        for i in range(0, min(5, len(subreddit.hot))):
+            post = subreddit.hot[i]
 
             # interact
+            self.viewed_posts += 1
+            post.views += 1
             self.vote(post)
 
             # reweigh bias
             self.new_bias(post)
 
 
-# build network
-# FIXME: using reduced complexity with n nodes, no subreddits but random connections
 class Network:
-    # change for network size and connectivity
-    user_number = 500
-    connection_number = 24000
-
-    # change properties of the Users
-    # TODO: connect the touch grass factor with user behavior
-    touch_grass_bias = 0.3
-    aa_fake_bias = 0.2
-    creator_bias = 0.03
-    check_bias = 0.1
-
-    aa_post_bias = 0
-
-    users = []
-    posts = []
-    connections = np.empty(user_number, dtype=object)
-
-    fake_biases = []
-    post_biases = []
-
-    post_fake_bias_sum = 0
-
-    # helper structures
-    duplicate_map = {}
-
-    # counts
-    connection_count = 0
-
     def __init__(self):
-        # build nodes (Users)
-        for _ in range(self.user_number):
-            self.users.append(User(self.aa_fake_bias, 0.1, self.creator_bias, 0.1, self.check_bias, 0.1))
+        # quantities
+        self.cnt_subreddits = 20
+        self.cnt_users = 2000
 
-        # build edges (Connections/Subreddits)
-        # FIXME: double connections exist
-        # TODO: connections should be weighted
-        for i in range(self.user_number):
-            self.connections[i] = []
+        # subreddit properties
+        self.sr_bias = 0.5
+        self.sr_tolerance = 0.4
 
-        # initializing duplicate map
-        for i in range(self.user_number):
-            self.duplicate_map[i] = []
+        # user properties
+        self.usr_bias = 0.5
+        self.usr_touch_grass_bias = 0.4
+        self.usr_creator_bias = 0.03
+        self.usr_subreddit_cap = 10
 
-        # generating random connections
-        connection_count = 0
-        while connection_count < self.connection_number:
-            # draw random user
-            u = np.random.randint(0, self.user_number)
+        # ls_s
+        self.ls_subreddits = []
+        self.ls_users = []
+        self.ls_posts = []
 
-            # and connect to other random user
-            v = random_connection(u, self.user_number)
+        # statistics
+        self.stats_post_bias_sum = 0.0
+        self.stats_user_bias_sum = 0.0
+        self.stats_biases = []
+        self.stats_post_biases = []
 
-            # check if we didn't already generate this connection
-            if v not in self.duplicate_map[u]:
-                self.duplicate_map[u].append(v)
-                connection_count += 1
+        # build subreddits
+        for _ in range(self.cnt_subreddits):
+            self.ls_subreddits.append(Subreddit(self.sr_bias, self.sr_tolerance))
 
-        self.connections = self.duplicate_map
+        # build users and give them a random assortment of subreddits
+        for _ in range(self.cnt_users):
+            user = User(self.usr_bias, self.usr_creator_bias, self.usr_touch_grass_bias)
+            user.subreddits = random.choices(self.ls_subreddits,
+                                             k=random.randint(1, min(self.cnt_subreddits, self.usr_subreddit_cap)))
+            self.ls_users.append(user)
 
     def simulate_round(self):
+        self.stats_user_bias_sum = 0.0
         # simulate users
-        user_fake_bias_sum = 0.0
-        for i, user in enumerate(self.users):
-            # TODO: return from own posts
-            user_fake_bias_sum += user.fake_bias
-            # TODO: case 0: touch grass
-            # case 1: create post
-            if random.random() < self.creator_bias:
+        for user in self.ls_users:
+            # TODO: add switching subreddits
+            # 0: sleep
+            if user.touch_grass_bias > random.random():
+                # TODO: reset tmp (doesn't exist yet)
+                pass
 
-                # user creates a post
+            # 1: create posts
+            elif user.creator_bias > random.random():
                 post = user.create_post()
-                self.posts.append(post)
-                self.post_fake_bias_sum += post.fake_bias
-                # and it lands in every buffer of his connections
-                for u in self.connections[i]:
-                    self.users[u].in_post.append(post)
+                self.stats_post_bias_sum += post.fake_bias
+                self.ls_posts.append(post)
 
-            # case 2: consume post
+            # 2: consume posts
             else:
                 user.consume_post()
-        self.aa_fake_bias = user_fake_bias_sum / self.user_number
 
-        # Do not ever iterate all posts! Horrible complexity!
-        # The average is calculated when creating the post
-        # TODO: kill post if too old (somewhere else, maybe in the consume queue)
-        self.aa_post_bias = self.post_fake_bias_sum / max(len(self.posts), 1)
+            # update statistics
+            self.stats_user_bias_sum += user.fake_bias
 
-        self.fake_biases.append(self.aa_fake_bias)
-        self.post_biases.append(self.aa_post_bias)
+        for subreddit in self.ls_subreddits:
+            # sort the hot lists
+            subreddit.hot.sort(key=get_hot, reverse=True)
+            # cut the hot lists to be the first [50] elements
+            subreddit.hot = subreddit.hot[0:50]
+
+        self.stats_biases.append(self.stats_user_bias_sum / len(self.ls_users))
+        self.stats_post_biases.append(self.stats_post_bias_sum / len(self.ls_posts))
 
 
 # run network for n rounds
 if __name__ == '__main__':
     # vars
     start_time = time.process_time()
-    rounds = 20  # in per_round
-    per_round = 500
+    rounds = 100  # in per_round
+    per_round = 480
     round_times = []
 
     # build
     my_reddit = Network()
     print(f"----------------------------------------\n"
           f"Simulating a Reddit-like Network with \n"
-          f"- {my_reddit.user_number} Users, \n"
-          f"- {my_reddit.connection_number} Connections and \n"
-          f"- a starting bias of {my_reddit.aa_fake_bias} \n"
+          f"- {my_reddit.cnt_users} Users, \n"
+          f"- {my_reddit.cnt_subreddits} Subreddits and \n"
+          f"- a starting user bias of {my_reddit.usr_bias} \n"
           f"for {rounds}x{per_round} time steps \n"
           f"---------------------------------------- \n")
 
     print(f"Data structures built in {time.process_time() - start_time} seconds")
 
     # plots in the beginning
-    plt.hist([u.fake_bias for u in my_reddit.users])
+    plt.hist([u.fake_bias for u in my_reddit.ls_users])
     plt.title("User Bias Histogram [start]")
     plt.show()
 
@@ -317,22 +273,22 @@ if __name__ == '__main__':
               f" {time.process_time() - round_timer} seconds")
 
         # plots after every round go here
-        plt.hist([u.fake_bias for u in my_reddit.users])
-        plt.title(f"User Bias Histogram [{i + 1}]")
-        plt.show()
+        # plt.hist([u.fake_bias for u in my_reddit.ls_users])
+        # plt.title(f"User Bias Histogram [{i + 1}]")
+        # plt.show()
 
     print(f"Simulation finished after {time.process_time() - start_time} seconds")
 
     # plot stuff
-    plt.plot(range(len(my_reddit.fake_biases)), my_reddit.fake_biases)
+    plt.plot(range(len(my_reddit.stats_biases)), my_reddit.stats_biases)
     plt.title("Average User Bias")
     plt.show()
 
-    plt.hist([u.fake_bias for u in my_reddit.users])
+    plt.hist([u.fake_bias for u in my_reddit.ls_users])
     plt.title("User Bias Histogram")
     plt.show()
 
-    plt.plot(range(1, len(my_reddit.post_biases)), my_reddit.post_biases[1:], 'r-')
+    plt.plot(range(1, len(my_reddit.stats_post_biases)), my_reddit.stats_post_biases[1:], 'r-')
     plt.title("Average Post Bias")
     plt.show()
 
